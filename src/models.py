235abb6acf312bcbdd2a5ac2324549f241d76586
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-21 15:24:25 +0000

    update imports

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()

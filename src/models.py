e38121b7a8fd279c543b7135f2dc3037854c0fa4
Dwane Dunn <dwane.dunn@gmail.com>
2025-06-21 15:28:29 +0000

    update the User model

--- src/models.py
+++ src/models.py
@@ -10,6 +10,10 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(
         String(120), unique=True, nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(80), unique=True, nullable=False)
+    first_name: Mapped[str] = mapped_column(String(80), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(80), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
